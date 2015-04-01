217acd9aa0d39ce8a676092e412fe831d2b4ebab
Lara Finnegan <lcfinnegan@gmail.com>
2015-03-31 23:38:02 -0400

    add sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
 
 	def new
+
 	end
 
 	def create
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			redirect_to root_url
+		else
+			render 'new'
+		end
 	end
 
 	def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 
 	has_many :posts
+
+	validates :name, presence: true
+	validates :username, presence: true
+	has_secure_password
 end
