d7a50d1d2c8086456dde33e4483cecf6f30adfc6
Lara Finnegan <lcfinnegan@gmail.com>
2015-04-02 22:11:06 -0400

    finish conditional authorization for logged in/logged out users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
-	before_action :logged_in_user, only: :index
+	before_action :logged_in_user, only: [:new, :create]
 
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -9,11 +10,19 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		@post.save
+		redirect_to root_url
 	end
 
 	private
 
 	def logged_in_user
 		redirect_to login_path unless logged_in?
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		log_out
+		logout
 		redirect_to root_url
 	end
 end
